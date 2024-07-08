a6d082112689d46afce3d8ea556fc82a613d726e
Yevhenii Baranik <baranik18@gmail.com>
2024-07-08 11:46:55 +0100

    edit

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,19 @@
-function getElementWidth(content, padding, border) {
-	console.log(getElementWidth);
+function checkForSpam(message) {
+	const lowerCaseMessage = message.toLowerCase();
 
-	return parseInt(content) + (parseInt(padding) * 2) + (parseInt(border)*2);
-};
+	if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Приклади викликів функції для перевірки її роботи
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message.")); // true
+console.log(checkForSpam("No spam here, just regular content.")); // true
+console.log(checkForSpam("Regular news and updates")); // false
+
